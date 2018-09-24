cb969d72c1f3301d622f0c4ed839bd941b9a2c99
Fursa Ilya <fursa.ilya@gmail.com>
2018-09-25 00:23:39 +0400

    Material Components BottomAppBar && Fab

--- app/src/main/java/com/todo/fursa/MainActivity.kt
+++ app/src/main/java/com/todo/fursa/MainActivity.kt
@@ -2,15 +2,30 @@ package com.todo.fursa
 
 import android.arch.lifecycle.Observer
 import android.os.Bundle
+import android.support.design.bottomappbar.BottomAppBar
+import android.support.design.widget.FloatingActionButton
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import android.widget.Toast
 import com.todo.fursa.room.database.AppDatabase
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var bottomAppBar: BottomAppBar
+    private lateinit var fab: FloatingActionButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottomAppBar = findViewById(R.id.bottom_appbar)
+        fab = findViewById(R.id.fab)
+
+        setSupportActionBar(bottomAppBar)
+
+        fab.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Clicked", Toast.LENGTH_SHORT).show()
+        }
+
 
         val db = AppDatabase.newInstance(baseContext)
         val dao = db!!.todoDAO

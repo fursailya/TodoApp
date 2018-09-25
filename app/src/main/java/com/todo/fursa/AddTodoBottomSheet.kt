c868f56ff7f36d716deab1dc407d8bb42e4ad6fb
Fursa Ilya <fursa.ilya@gmail.com>
2018-09-25 17:33:47 +0400

    BottomSheet design

--- app/src/main/java/com/todo/fursa/AddTodoBottomSheet.kt
+++ app/src/main/java/com/todo/fursa/AddTodoBottomSheet.kt
@@ -1,17 +1,31 @@
 package com.todo.fursa
 
 import android.os.Bundle
+import android.support.design.button.MaterialButton
 import android.support.design.widget.BottomSheetDialogFragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
 open class AddTodoBottomSheet: BottomSheetDialogFragment() {
+    private lateinit var btnAdd: MaterialButton
+    private lateinit var btnDismiss: MaterialButton
+    private lateinit var rootView: View
 
     override fun getTheme(): Int = R.style.BottomSheetDialogTheme
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return layoutInflater.inflate(R.layout.bottom_sheet_add_todo, container, false)
+        rootView = layoutInflater.inflate(R.layout.bottom_sheet_add_todo, container, false)
+        btnAdd = rootView.findViewById(R.id.btnAdd)
+        btnDismiss = rootView.findViewById(R.id.btnDismiss)
+
+        btnAdd.setOnClickListener {
+
+        }
+
+        btnDismiss.setOnClickListener { dismiss() }
+
+        return rootView
     }
 
     companion object {
